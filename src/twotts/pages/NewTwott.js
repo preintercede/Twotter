5b03302967f5f28bd10cd7b65cd741bbc8f4940b
Dylan <email@example.com>
2020-05-26 13:45:43 -0700

    added twott title and description form

--- src/twotts/pages/NewTwott.js
+++ src/twotts/pages/NewTwott.js
@@ -1,18 +1,90 @@
-import React from "react";
+import React, { useCallback, useReducer } from "react";
 import Input from "../../shared/components/FormElements/Input";
-import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
 import "./NewTwott.css";
+import Button from "../../shared/components/FormElements/Button";
+
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid },
+        },
+        isValid: formIsValid,
+      };
+
+    default:
+      return state;
+  }
+};
 
 const NewTwott = () => {
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: "",
+        isValid: false,
+      },
+    },
+    description: {
+      value: "",
+      isValid: false,
+    },
+    isValid: false,
+  });
+
+  const inputHandler = useCallback((id, value, isValid) => {
+    dispatch({
+      type: "INPUT_CHANGE",
+      value: value,
+      isValid: isValid,
+      inputId: id,
+    });
+  }, []);
+
+  const twottSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
+
   return (
-    <form className="twott-form">
+    <form className="twott-form" onSubmit={twottSubmitHandler}>
       <Input
+        id="title"
         element="input"
         type="text"
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title"
+        onInput={inputHandler}
       />
+      <Input
+        id="description"
+        element="textarea"
+        type="description"
+        label="Title"
+        validators={[VALIDATOR_MINLENGTH(1)]}
+        errorText="Please enter a description"
+        onInput={inputHandler}
+      />
+
+      <Button type="submit" disabled={!formState.isValid}>
+        Add Twott
+      </Button>
     </form>
   );
 };
