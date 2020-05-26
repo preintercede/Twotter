b1d5a65b8b7d9e35f5505314f8cda390e2d008cc
Dylan <email@example.com>
2020-05-25 23:40:57 -0700

    add input form text with validations

--- src/twotts/pages/NewTwott.js
+++ src/twotts/pages/NewTwott.js
@@ -1,6 +1,20 @@
 import React from "react";
+import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import "./NewTwott.css";
 
 const NewTwott = () => {
-  return <h1>Twotts here</h1>;
+  return (
+    <form className="twott-form">
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title"
+      />
+    </form>
+  );
 };
+
 export default NewTwott;
