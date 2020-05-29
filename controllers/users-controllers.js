4642a45a673848fc72cac96dbc1e8a90fcbe598f
Dylan <email@example.com>
2020-05-28 21:49:26 -0700

    generated hashedpassword and created jwt

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,6 +1,8 @@
 const { validationResult } = require("express-validator");
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const getUsers = async (req, res, next) => {
   let users;
@@ -37,12 +39,20 @@ const signup = async (req, res, next) => {
     return next(error);
   }
 
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(password, 12); //12 is num of salt rounds
+  } catch (err) {
+    const error = new HttpError("Could not create user. Please try again", 500);
+    return next(error);
+  }
+
   const createdUser = new User({
     name,
     email,
     image:
       "https://www.creativefreedom.co.uk/wp-content/uploads/2017/06/Twitter-featured.png",
-    password,
+    password: hashedPassword,
     twotts: [],
   });
 
@@ -67,7 +77,23 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
-  if (!existingUser || existingUser.password !== password) {
+  if (!existingUser) {
+    const error = new HttpError("Invalid credentials", 401);
+    return next(error);
+  }
+
+  let isValidPassword = false;
+  try {
+    isValidPassword = await bcrypt.compare(password, existingUser.password);
+  } catch (err) {
+    const error = new HttpError(
+      "Could not log you in. Please check your credentials",
+      500
+    );
+    return next(error);
+  }
+
+  if (!isValidPassword) {
     const error = new HttpError("Invalid credentials", 401);
     return next(error);
   }
